e67754fefc457ebdbacc06ef1c8b0c4b714d4466
HenryLiu714 <henryliu714@gmail.com>
2026-04-10 12:02:39 -0400

    feat: add event types

--- src/types/EventTypes.ts
+++ src/types/EventTypes.ts
@@ -1,13 +1,41 @@
 
-enum EventStatus {
-    DRAFT = 'DRAFT',
-    PUBLISHED = 'PUBLISHED',
-    CANCELLED = 'CANCELLED',
-    CONCLUDED = 'CONCLUDED',
+export type EventStatus = 'DRAFT' | 'PUBLISHED' | 'CANCELLED' | 'CONCLUDED';
+
+export type RSVPStatus = 'YES' | 'NO' | 'WAITLIST';
+
+export interface IEventAttendee {
+    event_id: number,
+    userId: string,
+    rsvpStatus: RSVPStatus,
+    createdAt: Date,
 }
 
-enum RSVPStatus {
-    YES = 'YES',
-    NO = 'NO',
-    WAITLIST = 'WAITLIST',
+export interface IUser {
+    id: string,
+    email: string,
+    attendees: IEventAttendee[],
 }
+
+export interface IEventDesc {
+    id: number,
+    eventId: number,
+    title: string,
+    desc: string,
+    location: string,
+    category: string,
+    datetime: Date,
+    organizerId: string,
+    capacity: number,
+    status: EventStatus,
+    createdAt: Date,
+    updatedAt: Date,
+}
+
+export interface IEvent {
+    id: number,
+    title: string,
+    createdAt: Date,
+    attendees: IEventAttendee[],
+    eventDesc: IEventDesc,
+}
+
