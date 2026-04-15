39af842e219dcfe91363b9c7a75d97e08c4d3f67
HenryLiu714 <henryliu714@gmail.com>
2026-04-15 14:42:43 -0400

    feat: add contracts for event creation service and controller

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -1,12 +1,27 @@
+import { Response } from "express";
 import { IEventService } from "../service/EventService";
 import { ILoggingService } from "../service/LoggingService";
 
 export interface IEventController {
-
+    showEventForm(res: Response): Promise<void>;
+    newEventFromForm(res: Response, 
+        form: {name: string,
+            description: string,
+            location: string,
+            datetime: string,
+            capacity: number}): Promise<void>;
 }
 
 class EventController implements IEventController {
     constructor(private readonly eventService: IEventService, private readonly logger: ILoggingService) {}
+
+    async showEventForm(res: Response): Promise<void> {
+        return;
+    }
+
+    async newEventFromForm(res: Response, form: {name: string; description: string; location: string; datetime: string; capacity: number}): Promise<void> {
+        return;
+    }
 }
 
 export function createEventController(eventService: IEventService, logger: ILoggingService): IEventController {
