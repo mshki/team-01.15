d6627fe7835acd5238d13baca265f487eb6db82d
mshki <132275360+mshki@users.noreply.github.com>
2026-04-15 21:54:16 -0400

    refactor: change logic for accepting fields when editing event

--- src/controllers/EventController.ts
+++ src/controllers/EventController.ts
@@ -3,14 +3,26 @@ import { IEventService } from "../service/EventService";
 import { ILoggingService } from "../service/LoggingService";
 import { IAppBrowserSession, IAuthenticatedUserSession } from "../session/AppSession";
 import { EventError } from "../lib/errors";
+import { IApp } from "../contracts";
 
 export interface IEventController {
-    editFromForm(res: Response, id: number, user: IAuthenticatedUserSession, session: IAppBrowserSession): Promise<void>;
+    getEditForm(res: Response, id: number, user: IAuthenticatedUserSession, session: IAppBrowserSession): Promise<void>;
+    editFromForm(
+        res: Response, 
+        id: number, 
+        user: IAuthenticatedUserSession, 
+        name: string,
+        description: string,
+        location: string,
+        startDatetime: Date,
+        endDatetime: Date,
+        capacity: number,
+        session: IAppBrowserSession): Promise<void>;
 }
 
 class EventController implements IEventController {
     constructor(private readonly eventService: IEventService, private readonly logger: ILoggingService) {}
-    
+
     private isEventError(value: unknown): value is EventError {
         return (
             typeof value === "object" &&
@@ -27,10 +39,14 @@ class EventController implements IEventController {
         return 500;
     }
     
-    async editFromForm(res: Response, id: number, user: IAuthenticatedUserSession, session: IAppBrowserSession): Promise<void> {
+    async getEditForm(res: Response, id: number, user: IAuthenticatedUserSession, session: IAppBrowserSession): Promise<void> {
         // TODO
     }
-    
+
+    async editFromForm(res: Response, id: number, user: IAuthenticatedUserSession, name: string, description: string, location: string, startDatetime: Date, endDatetime: Date, capacity: number, session: IAppBrowserSession): Promise<void> {
+        // TODO
+    }
+
 }
 
 export function createEventController(eventService: IEventService, logger: ILoggingService): IEventController {
