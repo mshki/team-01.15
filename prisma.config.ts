1012a448edee75ed3e59907416fbb959466cc710
HenryLiu714 <henryliu714@gmail.com>
2026-04-28 08:39:28 -0400

    refactor: fix prisma url

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,16 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+import Database from "better-sqlite3";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const dbUrl = process.env.DATABASE_URL?.replace(/^file:/, "") ?? "src/db/data.db";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  datasource: {
-    url: env("DATABASE_URL"),
+  migrate: {
+    adapter: () => {
+      const db = new Database(dbUrl);
+      return new PrismaBetterSqlite3(db);
+    },
   },
 });
