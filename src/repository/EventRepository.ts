85eacbfcd9f71b02257ef6df806aaf81d01fecde
mshki <132275360+mshki@users.noreply.github.com>
2026-04-29 02:21:21 -0400

    feat: add saveRsvp repo function to ensure rsvp persistence

--- src/repository/EventRepository.ts
+++ src/repository/EventRepository.ts
@@ -1,7 +1,7 @@
 
 import { EventError } from "../lib/errors";
 import { Result } from "../lib/result";
-import { CreateEventData, IEvent, IRSVP, } from "../types/EventTypes";
+import { CreateEventData, IEvent, IRSVP, RSVPStatus, } from "../types/EventTypes";
 
 export interface IEventRepository {
     getAllEvents(): Promise<Result<IEvent[], EventError>>;
@@ -10,4 +10,5 @@ export interface IEventRepository {
     updateEvent(id: number, event: Partial<IEvent>): Promise<Result<IEvent, EventError>>;
     deleteEvent(id: number): Promise<Result<void, EventError>>;
     findUserRsvp(id: number, userId: string): Promise<Result<IRSVP | null, EventError>>;
+    saveRsvp(id: number, userId: string, status: RSVPStatus): Promise<Result<void, EventError>>;
 }
